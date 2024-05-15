4d3f5b72314229278f5302fc040abc8a9c19da73
Emil2103 <emilsaubanov2013@gmail.com>
2024-05-15 14:02:52 +0300

    Добавил алгоритм заполнения NaN, а также функцию для расчета RMSE

--- utils/nans_handler.py
+++ utils/nans_handler.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
-
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import KNNImputer
+from sklearn.metrics import mean_squared_error
 
 def create_nan(data: pd.DataFrame, prob: float=0.5) -> pd.DataFrame:
     """
@@ -44,3 +46,42 @@ def fill_nans_mean(data: pd.DataFrame) -> pd.DataFrame:
             filled_data.loc[prev_index+1:next_index-1, column] = mean_value
             
     return filled_data
+
+
+def fill_missing_values(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Интерполяция по времени позволяет заполнить пропуски, 
+    используя соседние известные значения и информацию o времени.
+    """
+
+    data['date'] = pd.to_datetime(data['date'])
+    data = data.set_index('date')
+    data = data.interpolate(method='time', limit_direction='both')
+
+    # Если вдруг остались незаполненные значения, то функция пытается заполнить их с помощью KNN, либо линейной регрессией
+
+    imputer = KNNImputer(n_neighbors=5)
+    data_filled = pd.DataFrame(imputer.fit_transform(data), columns=data.columns, index=data.index)
+
+    for column in data_filled.columns[data_filled.isnull().any()]:
+        missing_indices = data_filled[column].index[data_filled[column].isnull()]
+        non_missing_indices = data_filled[column].index[~data_filled[column].isnull()]
+        X = non_missing_indices.to_numpy().reshape(-1, 1)
+        y = data_filled.loc[non_missing_indices, column].to_numpy().reshape(-1, 1)
+        model = LinearRegression()
+        model.fit(X, y)
+        missing_values = model.predict(missing_indices.to_numpy().reshape(-1, 1))
+        data_filled.loc[missing_indices, column] = missing_values.flatten()
+
+    data_filled.reset_index(inplace=True)
+
+    return data_filled
+
+
+def calculate_rmse(original_df: pd.DataFrame, filled_df: pd.DataFrame) -> float:
+    """
+    original_df - исходный dataframe без пропусков
+    filled_df - dataframe с заполненными пропусками
+    """
+    numeric_columns = original_df.select_dtypes(include=np.number).columns
+    return np.sqrt(mean_squared_error(original_df[numeric_columns].values.ravel(), filled_df[numeric_columns].values.ravel()))
