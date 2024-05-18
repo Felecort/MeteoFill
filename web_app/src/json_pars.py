f9446b90edf6c14b07556039bdf61e4774526e5c
vadim <dotpointen@gmail.com>
2024-05-18 17:42:06 +0300

    DELETED ZERO IDX! update_data eccept two dataframes

--- web_app/src/json_pars.py
+++ web_app/src/json_pars.py
@@ -15,30 +15,30 @@ def calculate_timestamps(start_timestamp: datetime, end_timestamp: datetime, del
 
 
 def parsing(data: dict) -> pd.DataFrame:
-    delay = data[0]["delay"]
-    start_timestamp = datetime.fromisoformat(data[0]["timestamps"]["start"])
-    end_timestamp = datetime.fromisoformat(data[0]["timestamps"]["end"])
+    delay = data["delay"]
+    start_timestamp = datetime.fromisoformat(data["timestamps"]["start"])
+    end_timestamp = datetime.fromisoformat(data["timestamps"]["end"])
     
     timestamps = calculate_timestamps(start_timestamp, end_timestamp, delay)
     
     df = pd.DataFrame({
         'time': timestamps,
-        'temperature_before': data[0]['data'][0]['values']['before'],
-        'temperature_after': data[0]['data'][0]['values']['after'],
-        'wind_speed_before': data[0]['data'][1]['values']['before'],
-        'wind_speed_after': data[0]['data'][1]['values']['after'],
-        'wind_direction_before': data[0]['data'][2]['values']['before'],
-        'wind_direction_after': data[0]['data'][2]['values']['after'],
-        'pressure_before': data[0]['data'][3]['values']['before'],
-        'pressure_after': data[0]['data'][3]['values']['after'],
-        'humidity_before': data[0]['data'][4]['values']['before'],
-        'humidity_after': data[0]['data'][4]['values']['after']
+        'temperature_before': data['data'][0]['values']['before'],
+        'temperature_after': data['data'][0]['values']['after'],
+        'wind_speed_before': data['data'][1]['values']['before'],
+        'wind_speed_after': data['data'][1]['values']['after'],
+        'wind_direction_before': data['data'][2]['values']['before'],
+        'wind_direction_after': data['data'][2]['values']['after'],
+        'pressure_before': data['data'][3]['values']['before'],
+        'pressure_after': data['data'][3]['values']['after'],
+        'humidity_before': data['data'][4]['values']['before'],
+        'humidity_after': data['data'][4]['values']['after']
     })
     return df
 
-def update_data(data: dict, data_frame_presents: pd.DataFrame) -> pd.DataFrame:
+def update_data(data: pd.DataFrame, data_frame_presents: pd.DataFrame) -> pd.DataFrame:
     max_rows = 50
-    df = parsing(data)
+    df = data
     result = pd.concat([data_frame_presents, df])
     result = result.reset_index(drop=True)
     if (len(result)>=max_rows):
