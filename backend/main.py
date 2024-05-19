d502fca577e7e5bdbdd35a968d8079aa704fb2ae
vadim <dotpointen@gmail.com>
2024-05-19 21:35:50 +0300

    explicit queue connection in main

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,12 @@
-from src import back_receiver
+
+from backend.src import back_consumer
 
 if __name__ == "__main__":
-    back_receiver.channel.start_consuming()
+    channel = back_consumer.get_channel()
+    # Consume messages from the 'data_queue'
+    channel.basic_consume(queue='data_queue',
+                        on_message_callback=back_consumer.callback,
+                        auto_ack=True)
+
+    print(' [x] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
