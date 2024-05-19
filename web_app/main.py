d5c5ad4b39fc9b50b6cc703ce6a3186551dd6b03
vadim <dotpointen@gmail.com>
2024-05-19 21:38:10 +0300

    refactor

--- web_app/main.py
+++ web_app/main.py
@@ -1,21 +1,21 @@
 
 from src import app
-from src import front_receiver, callback
 from time import sleep
 from multiprocessing import Process
+from web_app.src import front_consumer
 
 
 def run_web():
     app.run(host="0.0.0.0", debug=True)
 
 
-def run_receiver():
+def run_consumer():
     print("Waiting...")
     sleep(10)
-    channel = front_receiver.get_channel()
+    channel = front_consumer.get_channel()
 
     channel.basic_consume(queue='gui_queue',
-                            on_message_callback=callback,
+                            on_message_callback=front_consumer.callback,
                             auto_ack=True)
     print(' [x] FRONT | Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
@@ -24,5 +24,5 @@ def run_receiver():
 if __name__ == "__main__":
     p1 = Process(target=run_web)
     p1.start()
-    p2 = Process(target=run_receiver)
+    p2 = Process(target=run_consumer)
     p2.start()
