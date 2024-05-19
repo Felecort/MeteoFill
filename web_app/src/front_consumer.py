623642f0dbac3bf26bcef5e65ac1c139374b9e71
vadim <dotpointen@gmail.com>
2024-05-19 22:40:00 +0300

    logic for consumer. Handle input data

--- web_app/src/front_consumer.py
+++ web_app/src/front_consumer.py
@@ -2,19 +2,27 @@
 import pika
 from time import sleep
 import json
-
+from src import json_pars
 
 # Global variable to store the received JSON data
 def callback(ch, method, properties, body):
     print(" [X] FRONT | Received JSON")
     
-    # Decode the message and store it in the global variable
-    json_data = body.decode('utf-8')
-    # json.dumps(json_data)
-    with open('actual_data.json', 'w') as f:
-        f.write(json_data)
-        print(" [X] FRONT | JSON Saved")
+    new_data = body.decode('utf-8')
+    new_data = json.loads(new_data)
+    new_data_df = json_pars.parsing(new_data)
+    
+    with open("response.json", "r") as f:
+        previous_data = json.load(f)
+        previous_data_df = json_pars.parsing(previous_data)
+    
 
+    ### EXPERIMENTAL
+    # FOR DEBUG PURPOSES ONLY
+    with open("response.json", "w") as f:
+        json.dump(new_data, f)
+    ### MUST BE IN "response.json"
+    # result = json_pars.update_data(new_data, previous_data)
 
 def get_channel():
     # Connect to RabbitMQ server running on localhost
