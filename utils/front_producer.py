81a18f408f96652ad56a8f4c2bc9fd9da3e2dd28
vadim <dotpointen@gmail.com>
2024-05-19 21:37:47 +0300

    front_sender -> front_producer

---
+++ utils/front_producer.py
@@ -0,0 +1,28 @@
+import pika
+import json
+
+
+def send_data(name="actual_data.json"):
+    # Connect to RabbitMQ server running on localhost
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    # Declare a queue named 'gui_queue'
+    channel.queue_declare(queue='gui_queue')
+
+    # Sample dataset
+    with open(f"web_app/{name}", 'r') as f:
+        dataset = json.load(f)
+
+    # Convert dataset to JSON string and send it to the 'gui_queue'
+    json_string = json.dumps(dataset)
+    channel.basic_publish(exchange='',
+                        routing_key='gui_queue',
+                        body=json_string)
+    print(" [X] FRONT | Dataset Sent")
+
+    connection.close()
+
+
+if __name__ == "__main__":
+    send_data()
