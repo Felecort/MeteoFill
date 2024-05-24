e59a34c8c5460502b50ad0f23d0887b5a1d92634
Шаронов Артем <46421404+Gizmotius@users.noreply.github.com>
2024-05-24 14:42:44 +0300

    обертывание web.py в класс

--- web_app/main.py
+++ web_app/main.py
@@ -1,10 +1,8 @@
-
-from src import app
+from src.web import WeatherApp
 from time import sleep
 from multiprocessing import Process
 from src import front_consumer
 import os
-
 
 RABBITMQ_LOCATION = os.environ["RABBITMQ_DEFAULT_LOCATION"]
 RABBITMQ_DEFAULT_USER = os.environ["RABBITMQ_DEFAULT_USER"]
@@ -13,25 +11,23 @@ RABBITMQ_DEFAULT_SERVER_NAME = os.environ["RABBITMQ_DEFAULT_SERVER_NAME"]
 
 SYSTEM_2_FRONTEND_QUEUE = os.environ["SYSTEM_2_FRONTEND"]
 
-
 def run_web():
-    app.run(host="0.0.0.0", debug=True)
-
+    weather_app = WeatherApp()
+    weather_app.run()#host="0.0.0.0", debug=True)
 
 def run_consumer():
     print("Waiting...")
     sleep(10)
     channel = front_consumer.get_channel(RABBITMQ_DEFAULT_USER,
-                                        RABBITMQ_DEFAULT_PASS,
-                                        RABBITMQ_DEFAULT_SERVER_NAME,
-                                        SYSTEM_2_FRONTEND_QUEUE)
+                                         RABBITMQ_DEFAULT_PASS,
+                                         RABBITMQ_DEFAULT_SERVER_NAME,
+                                         SYSTEM_2_FRONTEND_QUEUE)
 
     channel.basic_consume(queue=SYSTEM_2_FRONTEND_QUEUE,
-                            on_message_callback=front_consumer.callback,
-                            auto_ack=True)
+                          on_message_callback=front_consumer.callback,
+                          auto_ack=True)
     print(' [x] FRONT | Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
-
 
 if __name__ == "__main__":
     p1 = Process(target=run_web)
--- web_app/src/web.py
+++ web_app/src/web.py
@@ -1,4 +1,3 @@
-
 from . import layout
 from . import json_pars
 
@@ -16,116 +15,125 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-external_stylesheets = [
-    {
-        "href": "https://fonts.googleapis.com/css2?"
-                "family=Lato:wght@400;700&display=swap",
-        "rel": "stylesheet",
-    },
-]
+class WeatherApp:
+    def __init__(self):
+        self.external_stylesheets = [
+            {
+                "href": "https://fonts.googleapis.com/css2?"
+                        "family=Lato:wght@400;700&display=swap",
+                "rel": "stylesheet",
+            },
+        ]
+        self.app = dash.Dash(__name__, external_stylesheets=self.external_stylesheets)
+        self.app.title = "Данные метеостанции!"
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+        self.data = pd.DataFrame(columns=['time',
+                                          'temperature_before',
+                                          'temperature_after',
+                                          'wind_speed_before',
+                                          'wind_speed_after',
+                                          'wind_direction_before',
+                                          'wind_direction_after',
+                                          'pressure_before',
+                                          'pressure_after',
+                                          'humidity_before',
+                                          'humidity_after'])
 
-app.title = "Данные метеостанции!"
+        self.app.layout = self.create_layout()
+        self.register_callbacks()
 
-data = pd.DataFrame(columns=['time',
-                                            'temperature_before',
-                                            'temperature_after',
-                                            'wind_speed_before',
-                                            'wind_speed_after',
-                                            'wind_direction_before',
-                                            'wind_direction_after',
-                                            'pressure_before',
-                                            'pressure_after',
-                                            'humidity_before',
-                                            'humidity_after'])
+    def create_layout(self):
+        return html.Div(children=[
+            layout.header(),
+            dcc.Interval(
+                id="interval-component",
+                interval=5000,  # в миллисекундах
+                n_intervals=0
+            ),
+            layout.charts(),
+            layout.data_table()
+        ])
 
-# Макет приложения
-app.layout = html.Div(children=[
-    layout.header(),
-    dcc.Interval(
-        id="interval-component",
-        interval=5000,  # в миллисекундах
-        n_intervals=0
-    ),
-    layout.charts(),
-    layout.data_table()
-])
+    def register_callbacks(self):
+        callback_args = ([Output('temp-chart', 'figure'),
+                          Output('pressure-chart', 'figure'),
+                          Output('humidity-chart', 'figure'),
+                          Output('wind-speed-chart', 'figure'),
+                          Output('wind-direction-chart', 'figure'),
+                          Output('data-table', 'data')],
+                         [Input('interval-component', 'n_intervals')])
 
-callback_args = ([Output('temp-chart', 'figure'),
-                Output('pressure-chart', 'figure'),
-                Output('humidity-chart', 'figure'),
-                Output('wind-speed-chart', 'figure'),
-                Output('wind-direction-chart', 'figure'),
-                Output('data-table', 'data')],
-                [Input('interval-component', 'n_intervals')])
+        @self.app.callback(*callback_args)
+        def update_charts(n_intervals):
+            with open("response.json", "r") as f:
+                raw_json = json.load(f)
+                if raw_json:
+                    self.data = json_pars.parsing(raw_json)
+                else:
+                    return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
 
+            temp_chart = self.create_temp_chart()
+            pressure_chart = self.create_pressure_chart()
+            humidity_chart = self.create_humidity_chart()
+            wind_speed_chart = self.create_wind_speed_chart()
+            wind_direction_chart = self.create_wind_direction_chart()
 
-@app.callback(*callback_args)
-def update_charts(n_intervals):
-    with open("response.json", "r") as f:
-        raw_json = json.load(f)
-        if raw_json:
-            data = json_pars.parsing(raw_json)
-        else:
-            return dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
+            return temp_chart, pressure_chart, humidity_chart, wind_speed_chart, wind_direction_chart, self.data.to_dict('records')
 
+    def create_temp_chart(self):
+        temp_chart = go.Figure()
+        temp_chart.add_trace(go.Scatter(x=self.data['time'], y=self.data['temperature_after'], mode='lines', name='Восстановленные данные', line=dict(color='red')))
+        temp_chart.add_trace(go.Scatter(x=self.data['time'], y=self.data['temperature_before'], mode='lines+markers', name='Полученные данные', line=dict(color='blue')))
+        temp_chart.update_layout(
+            xaxis_tickformat='%Y-%m-%d %H:%M',
+            title='Температура'
+        )
+        return temp_chart
 
-    # with open("actual_data.json", "r") as f:
-    #     raw_data = json.load(f)
-    #     data = json_pars.parsing(raw_data)
-    # with open("past_data.json", "r") as f:
-    #     data_frame_presents = json_pars.parsing(json.load(f))
-    
-    # data_frame_presents = json_pars.update_data(data, data_frame_presents)
+    def create_pressure_chart(self):
+        pressure_chart = go.Figure()
+        pressure_chart.add_trace(go.Scatter(x=self.data['time'], y=self.data['pressure_after'], mode='lines', name='Восстановленные данные', line=dict(color='red')))
+        pressure_chart.add_trace(go.Scatter(x=self.data['time'], y=self.data['pressure_before'], mode='lines+markers', name='Полученные данные', line=dict(color='blue')))
+        pressure_chart.update_layout(
+            xaxis_tickformat='%Y-%m-%d %H:%M',
+            title='Давление'
+        )
+        return pressure_chart
 
-    # with open("past_data.json", "w") as f: 
-    #     json.dump(raw_data, f)
+    def create_humidity_chart(self):
+        humidity_chart = go.Figure()
+        humidity_chart.add_trace(go.Scatter(x=self.data['time'], y=self.data['humidity_after'], mode='lines', name='Восстановленные данные', line=dict(color='red')))
+        humidity_chart.add_trace(go.Scatter(x=self.data['time'], y=self.data['humidity_before'], mode='lines+markers', name='Полученные данные', line=dict(color='blue')))
+        humidity_chart.update_layout(
+            xaxis_tickformat='%Y-%m-%d %H:%M',
+            title='Влажность'
+        )
+        return humidity_chart
 
-    
-    temp_chart = go.Figure()
-    temp_chart.add_trace(go.Scatter(x=data['time'], y=data['temperature_after'], mode='lines', name='Восстановленные данные', line=dict(color='red')))
-    temp_chart.add_trace(go.Scatter(x=data['time'], y=data['temperature_before'], mode='lines+markers', name='Полученные данные', line=dict(color='blue')))
-    temp_chart.update_layout(
-        xaxis_tickformat='%Y-%m-%d %H:%M',
-        title='Температура'
-    )
+    def create_wind_speed_chart(self):
+        wind_speed_chart = go.Figure()
+        wind_speed_chart.add_trace(go.Scatter(x=self.data['time'], y=self.data['wind_speed_after'], mode='lines', name='Восстановленные данные', line=dict(color='red')))
+        wind_speed_chart.add_trace(go.Scatter(x=self.data['time'], y=self.data['wind_speed_before'], mode='lines+markers', name='Полученные данные', line=dict(color='blue')))
+        wind_speed_chart.update_layout(
+            xaxis_tickformat='%Y-%m-%d %H:%M',
+            title='Скорость ветра'
+        )
+        return wind_speed_chart
 
-    pressure_chart = go.Figure()
-    pressure_chart.add_trace(go.Scatter(x=data['time'], y=data['pressure_after'], mode='lines', name='Восстановленные данные', line=dict(color='red')))
-    pressure_chart.add_trace(go.Scatter(x=data['time'], y=data['pressure_before'], mode='lines+markers', name='Полученные данные', line=dict(color='blue')))
-    pressure_chart.update_layout(
-        xaxis_tickformat='%Y-%m-%d %H:%M',
-        title='Давление'
-    )
+    def create_wind_direction_chart(self):
+        wind_direction_chart = go.Figure()
+        wind_direction_chart.add_trace(go.Scatter(x=self.data['time'], y=self.data['wind_direction_after'], mode='lines', name='Востановленные данные', line=dict(color='red')))
+        wind_direction_chart.add_trace(go.Scatter(x=self.data['time'], y=self.data['wind_direction_before'], mode='lines+markers', name='Полученные данные', line=dict(color='blue')))
+        wind_direction_chart.update_layout(
+            xaxis_tickformat='%Y-%m-%d %H:%M',
+            title='Направление ветра'
+        )
+        return wind_direction_chart
 
-    humidity_chart = go.Figure()
-    humidity_chart.add_trace(go.Scatter(x=data['time'], y=data['humidity_after'], mode='lines', name='Восстановленные данные', line=dict(color='red')))
-    humidity_chart.add_trace(go.Scatter(x=data['time'], y=data['humidity_before'], mode='lines+markers', name='Полученные данные', line=dict(color='blue')))
-    humidity_chart.update_layout(
-        xaxis_tickformat='%Y-%m-%d %H:%M',
-        title='Влажность'
-    )
-
-    wind_speed_chart = go.Figure()
-    wind_speed_chart.add_trace(go.Scatter(x=data['time'], y=data['wind_speed_after'], mode='lines', name='Восстановленные данные', line=dict(color='red')))
-    wind_speed_chart.add_trace(go.Scatter(x=data['time'], y=data['wind_speed_before'], mode='lines+markers', name='Полученные данные', line=dict(color='blue')))
-    wind_speed_chart.update_layout(
-        xaxis_tickformat='%Y-%m-%d %H:%M',
-        title='Скорость ветра'
-    )
-
-    wind_direction_chart = go.Figure()
-    wind_direction_chart.add_trace(go.Scatter(x=data['time'], y=data['wind_direction_after'], mode='lines', name='Востановленные данные', line=dict(color='red')))
-    wind_direction_chart.add_trace(go.Scatter(x=data['time'], y=data['wind_direction_before'], mode='lines+markers', name='Полученные данные', line=dict(color='blue')))
-    wind_direction_chart.update_layout(
-        xaxis_tickformat='%Y-%m-%d %H:%M',
-        title='Направление ветра'
-    )
-
-    # Возвращение графиков и таблицы
-    return temp_chart, pressure_chart, humidity_chart, wind_speed_chart, wind_direction_chart, data.to_dict('records')
+    def run(self):
+        self.app.run_server(host="0.0.0.0", debug=True)
 
 
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", debug=True)
+    weather_app = WeatherApp()
+    weather_app.run()
