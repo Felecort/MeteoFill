ae4ff396825d7ad1345537cdbf273879a9e587de
Vadim <dopointen@gmail.com>
2024-10-29 22:24:14 +0300

    update default external location

--- control-system/main.py
+++ control-system/main.py
@@ -1,6 +1,14 @@
 from src import BusinessLogic
 from time import sleep
 import os
+import socket
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip_address = s.getsockname()[0]
+s.close()
+
 
 RABBITMQ_LOCATION = os.environ["RABBITMQ_DEFAULT_LOCATION"]
 RABBITMQ_DEFAULT_USER = os.environ["RABBITMQ_DEFAULT_USER"]
@@ -10,11 +18,20 @@ RABBITMQ_DEFAULT_SERVER_NAME = os.environ["RABBITMQ_DEFAULT_SERVER_NAME"]
 BACKEND_2_SYSTEM_QUEUE = os.environ["BACKEND_2_SYSTEM"]
 SYSTEM_2_BACKEND_QUEUE = os.environ["SYSTEM_2_BACKEND"]
 SYSTEM_2_FRONTEND_QUEUE = os.environ["SYSTEM_2_FRONTEND"]
-METEOSTATION_LOCATION = os.environ["METEOSTATION_LOCATION"]
-EXTERNAL_LOCATION = os.environ["EXTERNAL_LOCATION"]
 
 REQUEST_FREQUENCY = int(os.environ["REQUEST_FREQUENCY"])
 
+try:
+    METEOSTATION_LOCATION = os.environ["METEOSTATION_LOCATION"]
+except KeyError:
+    METEOSTATION_LOCATION = 'http://' + ip_address + ':8092/process-csv'
+
+
+try:
+    EXTERNAL_LOCATION = os.environ["EXTERNAL_LOCATION"]
+except KeyError:
+    EXTERNAL_LOCATION = 'http://' + ip_address + ':8092/rmse'
+    
 
 if __name__ == '__main__':
     sleep(12)
