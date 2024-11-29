05f86962c6ae8452115e730468318a032e500a4d
Felecort <dotpointen@gmail.com>
2024-11-30 00:56:22 +0300

    auto detect platform

--- test-system/endpoint.py
+++ test-system/endpoint.py
@@ -242,7 +242,22 @@ async def process_csv():
         raise HTTPException(status_code=500, detail=str(e))
 
 
+def get_current_ip():
+    import socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    tmp_ip = s.getsockname()[0]
+    s.close()
+    return tmp_ip
+
+
 if __name__ == '__main__':
+    import platform
+    if platform.system() == 'Windows':
+        ip = '127.0.0.1'
+    else:
+        ip = get_current_ip()
+
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8092)
+    uvicorn.run(app, host=ip, port=8092)
 
